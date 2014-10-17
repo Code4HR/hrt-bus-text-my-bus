099bb326d7a3d29ae7863265528dabeac16ac3a0
stanley zheng <dev.stanzheng@gmail.com>
2014-10-17 16:46:34 -0400

    -A

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var app = express();
 var path = require('path');
  twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+var moment = require('moment');
+var _ = require('lodash');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -10,10 +12,12 @@ var data = [];
 app.post('/text', function (req,res) {
 
   console.log(req.body);
+  console.log(req.headers);
   console.log(req.params);
   data.push(req.body);
   data.push(req.params);
   res.send(data);
+
   // res.send();
   // twilio.sendMessage({
   //
@@ -36,25 +40,37 @@ app.get('/', function (req,res) {
   res.redirect(301, 'http://hrtb.us');
 });
 
-
+//R
 
 app.get('/notify/:bus/:number', function (req,res) {
   var api = "http://api.hrtb.us/api/stop_times/";
   var bus = req.params.bus;
-  r.get(api+bus, function (err, response, body) {
-    console.log(api+bus);
-    res.send(body);
-  });
-
 });
 
 app.post('/msg', function (req,res) {
-  res.send("Welcome to HRTB.us A volunteer project created by Code4HR. http://code4hr.org Just a moment, we're getting your bus stop information.");
+  res.send(getResponse());
 });
 
+//EVMS/NORFOLK will be here in about 10, the next one in 15
+//NEWTOWN ROAD will be here in about 5 minutes and the next
 
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+function test() {
+  r.get("http://api.hrtb.us/api/stop_times/8004", function (err, response, body) {
+    console.log("http://api.hrtb.us/api/stop_times/8004");
+    console.log();
+    var test = _.groupBy(JSON.parse(body),"destination");
+    console.log(test);
+  });
+}
+
+function getResponse(message) {
+  return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n<Message>" + "buddy!" + "</Message>\n</Response>";
+}
+
+test();
