bc701e5eb8b9810f89978c144e222ef4ebca3705
stanley zheng <dev.stanzheng@gmail.com>
2014-10-17 09:39:45 -0400

    sending to the internet

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get('/', function (req,res) {
 
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + server.address().port);
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
 });
