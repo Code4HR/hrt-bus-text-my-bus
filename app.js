d6183195c5fbd34696c05de2d9e372110fafc562
stanley zheng <dev.stanzheng@gmail.com>
2014-10-17 09:12:13 -0400

    adding my response

--- app.js
+++ app.js
@@ -4,20 +4,6 @@ var app = express();
 var api = "http://api.hrtb.us/api/stop_times/8004";
  twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-//
-//
-//  var resp = new twilio.TwimlResponse();
-//
-// resp.say('Welcome to Twilio!');
-// resp.say('Please let us know if we can help during your development.', {
-//     voice:'woman',
-//     language:'en-gb'
-// });
-
-
-
-
-
 // r.get(api, function (err, res, body) {
 //   console.log(body);
 // });
