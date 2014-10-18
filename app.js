c4222cbbfc908a662ef08bd1d4b00235c633ef8f
stanley zheng <dev.stanzheng@gmail.com>
2014-10-18 03:08:16 -0400

    removd unused code

--- app.js
+++ app.js
@@ -8,48 +8,13 @@ var _ = require('lodash');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var data = [];
-app.post('/text', function (req,res) {
-
-  console.log(req.body);
-  console.log(req.headers);
-  console.log(req.params);
-  data.push(req.body);
-  data.push(req.params);
-  res.send(data);
-
-  // res.send();
-  // twilio.sendMessage({
-  //
-  //     to: number, // Any number Twilio can deliver to
-  //     from: '+17579135000', // A number you bought from Twilio and can use for outbound communication
-  //     body: 'businfo' + name + ": " + message,
-  // }, function(err, responseData) {
-  //     console.log(err);
-  //     if (!err) {
-  //     console.log(responseData);
-  //     res.send(responseData.from + " " + responseData.body);
-  //     res.end();
-  //     }
-  // });
-});
-
 // serve public
-
 app.get('/', function (req,res) {
   res.redirect(301, 'http://hrtb.us');
 });
 
-//R
-
-app.get('/notify/:bus/:number', function (req,res) {
-  var api = "http://api.hrtb.us/api/stop_times/";
-  var bus = req.params.bus;
-});
-
 app.post('/msg', function (req,res) {
-  console.log(req.body.Body);
-  res.send(getResponse(req.body.Body));
+  res.send(getResponse(req.query.Body));
 });
 
 //EVMS/NORFOLK will be here in about 10, the next one in 15
@@ -61,17 +26,16 @@ app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-function test() {
+function getResponse(message) {
+  return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n<Message>" + message + "</Message>\n</Response>";
+}
+
+function debugging() {
   r.get("http://api.hrtb.us/api/stop_times/8004", function (err, response, body) {
     console.log("http://api.hrtb.us/api/stop_times/8004");
-    console.log();
     var test = _.groupBy(JSON.parse(body),"destination");
     console.log(test);
   });
 }
 
-function getResponse(message) {
-  return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n<Message>" + message + "</Message>\n</Response>";
-}
-
-test();
+debugging();
