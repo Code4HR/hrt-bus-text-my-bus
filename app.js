1dc7f27ebb7d0ae02c26c8efdbf415624763c452
Ryan Y <ryayak1460@protingumas.com>
2014-11-09 01:14:59 -0500

    Removed commented out code.  Included a friendly error message for routes without stops.  Updated the time difference code to calculate time til the next stop.  Refactored to reduce variable usage, increase functional programming presence and reduce function size to less than 20 lines per function.

--- app.js
+++ app.js
@@ -1,161 +1,121 @@
-var r = require("request");
-var express = require("express");
-var app = express();
-var path = require('path');
-//var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-var twilio = require('twilio');
-var moment = require('moment');
-var _ = require('lodash');
-var qs = require('querystring');
-var bodyParser = require('body-parser');
-var bPromise = require('bluebird');
+var r = require("request"),
+    express = require("express"),
+    app = express(),
+    path = require('path'),
+    //twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN),
+    twilio = require('twilio'),
+    moment = require('moment'),
+    _ = require('lodash'),
+    // qs = require('querystring'),
+    bodyParser = require('body-parser'),
+    bPromise = require('bluebird');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
+
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// serve public
+// Serve public.
 app.get('/', function (req,res) {
-  res.redirect(301, 'http://hrtb.us');
+    res.redirect(301, 'http://hrtb.us');
 });
 
 
-// example curl from twilio
+// Example curl from twilio.
 /**  curl -X POST -d "From=3031111111&Body=Test Message" http://localhost:3000/msg **/
-
-app.post('/msg', function (req,res) {
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  console.log("****DEBUGGING***");
-  console.log(req.headers);
-  console.log(req.query);
-  console.log(req.body);
-  console.log("form",req.form);
-  console.log(req.params);
-//  console.log(req.params.Body);
-
-
-  //TRANSFORM THIS DATA
-  var busstop = req.body.Body;
-
-  res.end(getResponse(req.body.Body));
+app.post('/msg', function (req, res) {
+    // TRANSFORM THIS DATA.
+    var busstop = req.body.Body;
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    console.log("****DEBUGGING***");
+    console.log(req.headers);
+    console.log(req.query);
+    console.log(req.body);
+    console.log("form",req.form);
+    console.log(req.params);
+    res.end(getResponse(req.body.Body));
 });
+
+/**
+ * Returns a promise which returns an XML response of the given message.
+ *
+ * @param {String} message - The message to return.
+ *
+ * @return {Promise} An XML promise.
+ **/
+var getResponse = function (message) {
+    return new bPromise(function (resolve, reject) {
+        resolve('<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n'
+            + '<Response>\n<Message>' + message + '</Message>\n</Response>');
+    });
+};
 
 app.get('/msg/:id', function (req, res) {
-
-  getStops(req.params.id).then(getResponse).then(res.send.bind(res));
-
+    getStops(req.params.id).then(getResponse).then(res.send.bind(res));
 });
 
+/** 
+ * Makes API request to the HRTB.US API and transform the json results.
+ *
+ * @param {String} param - The parameter to get stops for.
+ *
+ * @return {Promise} A promise returning the stringified hrtb.us results,
+ *      passing them into the continuation.
+ **/
+var getStops = function (param) {
+    return new bPromise(function (resolve, reject) {
+        r.get("http://api.hrtb.us/api/stop_times/" 
+            + (_.isUndefined(param) ? 
+                '8004' :
+                param),
+            function (err, response, body) {
+                var stops = body === '[]' || JSON.parse(body) === [] ?
+                    'Hmm, I cannot tell if that stop does not exist or if no '
+                        + 'buses will come to that stop in the near future.\n'
+                        + 'Either way, no buses will come to the stop you '
+                        + 'mentioned any time soon.' :
+                    parseStops(body, resolve);
+                console.log(stops);
+                return resolve(stops);
+            });
+    }); 
+};
 
-//EVMS/NORFOLK will be here in about 10, the next one in 15
-//NEWTOWN ROAD will be here in about 5 minutes and the next
+/**
+ * Parse out the steps, output them and run a continuation on them.
+ *
+ * @param {String} body - The body of the response.
+ *
+ * @return {String} The stops in a string format.
+ **/
+var parseStops = function (body) {
+    // Sort by routes and destinations.
+    return _.map(_.groupBy(JSON.parse(body), 'destination'),
+        function (stops) {
+            return _.reduce(stops, function (response, stop) {
+                return { 
+                          '0': 'Light rail'
+                        , '3': 'Bus'
+                        , '4': 'Ferry' 
+                    }[stop.drop_off_type] + ' '  
+                    + stop.routeShortName + ' to ' + stop.destination + ' in ' 
+                    + moment.utc(stop.arrival_time).diff(moment.utc()
+                        , 'minutes') 
+                    + ' minutes.';
+            }, '');
+        }).join('\n');
+};
 
+// EVMS/NORFOLK will be here in about 10, the next one in 15.
+// NEWTOWN ROAD will be here in about 5 minutes and the next.
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
-function getResponse(message) {
-  return new bPromise(function (resolve, reject) {
-  resolve("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n<Message>" + message + "</Message>\n</Response>");
-  });
-}
-
 //http://hrtb.us/#stops/0263
-
-var	routeType = {
-  '0': 'light-rail',
-  '3': 'bus',
-  '4': 'ferry'
-};
-//
-// var date = function(arrival_time) {
-//   return new Date(Date.toISOString(arrival_time));
-// };
-// var calculateTime = function (busAdherence) {
-//   var arriveTime = date();
-//   if(busAdherence) {
-//     arriveTime = arriveTime.addMinutes(busAdherence * -1);
-//   }
-//
-//   var arriveTimeFromNow = new Date(arriveTime - new Date().getTime());
-//   return (arriveTimeFromNow.getTime() / 1000 / 60 | 0);
-// };
-
-/**Makes API request to the HRTB.US API and transform the json results **/
-
-function getStops(param) {
-  param = "8004";
-  console.log(param);
-  if (_.isUndefined(param)){
-     param = "8004";
-  }
-  return new bPromise(function (resolve, reject) {
-
-  var url = "http://api.hrtb.us/api/stop_times/"+ param;
-  r.get(url, function (err, response, body) {
-    console.log(JSON.parse(body));
-    console.log(body);
-    console.log(typeof JSON.parse(body));
-
-    if (JSON.parse(body) === []){
-      return resolve("not a valid stop");
-    }
-    var info = [];
-    var stops = {};
-    var destinations = _.groupBy(JSON.parse(body), "destination");
-    var routes;
-    //get the time now
-    var now = moment.utc(new Date());
-    //sort by routes and destinations
-    _.each(destinations, function (route) {
-      _.each(route, function (stop, key) {
-      var time = moment(stop.arrival_time).diff(now);
-      // TODO DANGEROUS -- DAYLIGHT SAVINGS TIME WILL BREAK THIS
-      var d = moment.duration(time).subtract("240","minutes");
-      time = Math.ceil(d.asMinutes()-240);
-        //new stop
-      if (_.isUndefined(stops[stop.destination]) && time > 0 ){
-          stops[stop.destination] = {
-            "route": stop.routeShortName,
-            "time": [time],
-            "type": routeType[stop.drop_off_type]
-          };
-      }
-      else if (time > 0 ){
-        //existing stop
-        stops[stop.destination].time.push(time);
-      }
-      });
-    });
-    console.log(toText(stops));
-    resolve(toText(stops));
-  });
-}); // end of promise
-}
-
-/** Stringify createed text object from from getStops **/
-function toText(stops) {
-  var response = '';
-  var aTimes;
-  var next;
-  _.each(stops, function (value, key){
-    next = value.time.slice(1);
-    if (next !== []) {
-       aTimes = _.reduce(next, function (a, b) {
-        return a + " & " + b;
-      });
-      next = "next " + value.type + " in "+ aTimes+ " mins";
-    }
-    else {
-      next = "";
-    }
-    response += value.type + " " +  value.route +  " to "+ key + " in " + value.time[0] + " mins "+ next + "\n";
-  });
-    return response;
-}
 
 getStops();
