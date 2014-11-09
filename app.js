546c5a4e3fb5c8f82853d06fe10a837fd7d47658
Ryan Y <ryayak1460@protingumas.com>
2014-11-09 01:26:56 -0500

    Included the license header.  Defined a function context for the server application.

--- app.js
+++ app.js
@@ -1,121 +1,137 @@
-var r = require("request"),
-    express = require("express"),
-    app = express(),
-    path = require('path'),
-    //twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN),
-    twilio = require('twilio'),
-    moment = require('moment'),
-    _ = require('lodash'),
-    // qs = require('querystring'),
-    bodyParser = require('body-parser'),
-    bPromise = require('bluebird');
+/*
+    Copyright {yyyy} {name of copyright owner}
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+        http://www.apache.org/licenses/LICENSE-2.0
 
-// Serve public.
-app.get('/', function (req,res) {
-    res.redirect(301, 'http://hrtb.us');
-});
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+(function () {
+    var r = require("request"),
+        express = require("express"),
+        app = express(),
+        path = require('path'),
+        //twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN),
+        twilio = require('twilio'),
+        moment = require('moment'),
+        _ = require('lodash'),
+        bodyParser = require('body-parser'),
+        bPromise = require('bluebird');
 
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
 
-// Example curl from twilio.
-/**  curl -X POST -d "From=3031111111&Body=Test Message" http://localhost:3000/msg **/
-app.post('/msg', function (req, res) {
-    // TRANSFORM THIS DATA.
-    var busstop = req.body.Body;
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    console.log("****DEBUGGING***");
-    console.log(req.headers);
-    console.log(req.query);
-    console.log(req.body);
-    console.log("form",req.form);
-    console.log(req.params);
-    res.end(getResponse(req.body.Body));
-});
+    app.use(bodyParser.json());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-/**
- * Returns a promise which returns an XML response of the given message.
- *
- * @param {String} message - The message to return.
- *
- * @return {Promise} An XML promise.
- **/
-var getResponse = function (message) {
-    return new bPromise(function (resolve, reject) {
-        resolve('<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n'
-            + '<Response>\n<Message>' + message + '</Message>\n</Response>');
+    // Serve public.
+    app.get('/', function (req,res) {
+        res.redirect(301, 'http://hrtb.us');
     });
-};
 
-app.get('/msg/:id', function (req, res) {
-    getStops(req.params.id).then(getResponse).then(res.send.bind(res));
-});
 
-/** 
- * Makes API request to the HRTB.US API and transform the json results.
- *
- * @param {String} param - The parameter to get stops for.
- *
- * @return {Promise} A promise returning the stringified hrtb.us results,
- *      passing them into the continuation.
- **/
-var getStops = function (param) {
-    return new bPromise(function (resolve, reject) {
-        r.get("http://api.hrtb.us/api/stop_times/" 
-            + (_.isUndefined(param) ? 
-                '8004' :
-                param),
-            function (err, response, body) {
-                var stops = body === '[]' || JSON.parse(body) === [] ?
-                    'Hmm, I cannot tell if that stop does not exist or if no '
-                        + 'buses will come to that stop in the near future.\n'
-                        + 'Either way, no buses will come to the stop you '
-                        + 'mentioned any time soon.' :
-                    parseStops(body, resolve);
-                console.log(stops);
-                return resolve(stops);
-            });
-    }); 
-};
+    // Example curl from twilio.
+    /**  curl -X POST -d "From=3031111111&Body=Test Message" http://localhost:3000/msg **/
+    app.post('/msg', function (req, res) {
+        // TRANSFORM THIS DATA.
+        var busstop = req.body.Body;
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        console.log("****DEBUGGING***");
+        console.log(req.headers);
+        console.log(req.query);
+        console.log(req.body);
+        console.log("form",req.form);
+        console.log(req.params);
+        res.end(getResponse(req.body.Body));
+    });
 
-/**
- * Parse out the steps, output them and run a continuation on them.
- *
- * @param {String} body - The body of the response.
- *
- * @return {String} The stops in a string format.
- **/
-var parseStops = function (body) {
-    // Sort by routes and destinations.
-    return _.map(_.groupBy(JSON.parse(body), 'destination'),
-        function (stops) {
-            return _.reduce(stops, function (response, stop) {
-                return { 
-                          '0': 'Light rail'
-                        , '3': 'Bus'
-                        , '4': 'Ferry' 
-                    }[stop.drop_off_type] + ' '  
-                    + stop.routeShortName + ' to ' + stop.destination + ' in ' 
-                    + moment.utc(stop.arrival_time).diff(moment.utc()
-                        , 'minutes') 
-                    + ' minutes.';
-            }, '');
-        }).join('\n');
-};
+    /**
+     * Returns a promise which returns an XML response of the given message.
+     *
+     * @param {String} message - The message to return.
+     *
+     * @return {Promise} An XML promise.
+     **/
+    var getResponse = function (message) {
+        return new bPromise(function (resolve, reject) {
+            resolve('<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n'
+                + '<Response>\n<Message>' + message + '</Message>\n</Response>');
+        });
+    };
 
-// EVMS/NORFOLK will be here in about 10, the next one in 15.
-// NEWTOWN ROAD will be here in about 5 minutes and the next.
-app.set('port', process.env.PORT || 3000);
+    app.get('/msg/:id', function (req, res) {
+        getStops(req.params.id).then(getResponse).then(res.send.bind(res));
+    });
 
-app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+    /** 
+     * Makes API request to the HRTB.US API and transform the json results.
+     *
+     * @param {String} param - The parameter to get stops for.
+     *
+     * @return {Promise} A promise returning the stringified hrtb.us results,
+     *      passing them into the continuation.
+     **/
+    var getStops = function (param) {
+        return new bPromise(function (resolve, reject) {
+            r.get("http://api.hrtb.us/api/stop_times/" 
+                + (_.isUndefined(param) ? 
+                    '8004' :
+                    param),
+                function (err, response, body) {
+                    var stops = body === '[]' || JSON.parse(body) === [] ?
+                        'Hmm, I cannot tell if that stop does not exist or if no '
+                            + 'buses will come to that stop in the near future.\n'
+                            + 'Either way, no buses will come to the stop you '
+                            + 'mentioned any time soon.' :
+                        parseStops(body, resolve);
+                    console.log(stops);
+                    return resolve(stops);
+                });
+        }); 
+    };
 
-//http://hrtb.us/#stops/0263
+    /**
+     * Parse out the steps, output them and run a continuation on them.
+     *
+     * @param {String} body - The body of the response.
+     *
+     * @return {String} The stops in a string format.
+     **/
+    var parseStops = function (body) {
+        // Sort by routes and destinations.
+        return _.map(_.groupBy(JSON.parse(body), 'destination'),
+            function (stops) {
+                return _.reduce(stops, function (response, stop) {
+                    return { 
+                              '0': 'Light rail'
+                            , '3': 'Bus'
+                            , '4': 'Ferry' 
+                        }[stop.drop_off_type] + ' '  
+                        + stop.routeShortName + ' to ' + stop.destination + ' in ' 
+                        + moment.utc(stop.arrival_time).diff(moment.utc()
+                            , 'minutes') 
+                        + ' minutes.';
+                }, '');
+            }).join('\n');
+    };
 
-getStops();
+    // EVMS/NORFOLK will be here in about 10, the next one in 15.
+    // NEWTOWN ROAD will be here in about 5 minutes and the next.
+    app.set('port', process.env.PORT || 3000);
+
+    app.listen(app.get('port'), function() {
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+
+    //http://hrtb.us/#stops/0263
+
+    getStops();
+}());
