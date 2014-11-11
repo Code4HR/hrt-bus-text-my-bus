6684cf27601410230991640de5124b84d6692524
Ryan Y <ryayak1460@protingumas.com>
2014-11-11 02:20:18 -0500

    Removed root route.  Removed 8004 as default stop for debugging.  Removed getStops call after starting the server.  Included stubbed functions with TODOs to create structure to write address text resolution.

--- app.js
+++ app.js
@@ -32,30 +32,88 @@
     app.use(bodyParser.json());
     app.use(express.static(path.join(__dirname, 'public')));
 
-    // Serve public.
-    app.get('/', function (req,res) {
-        res.redirect(301, 'http://hrtb.us');
+    // Example curl from twilio.
+    /* curl -X POST -d "From=7571112222&Body=8004" http://localhost:3000/msg */
+    app.post('/msg', function (req, res) {
+        logRequestDebuggingValues(req);
+
+        // Checks for a stop number, like http://hrtb.us/#stops/0263
+        (isFinite(parseInt(req.body.Body)) ? 
+            getStops(req.body.Body).then(getResponse) : 
+         // Check for an address.
+         hasAddress(req.body.Body) ?
+            getAddress(req.body.Body).then(getResponse) :
+            // Otherwise, give a help(ful) message.
+            getResponse('Hi, thanks for texting your HRT bus! Please text a ' 
+                 + 'stop number to find the next time your bus will come your '
+                 + 'way.')).then(res.send.bind(res));
     });
 
-
-    // Example curl from twilio.
-    /**  curl -X POST -d "From=3031111111&Body=Test Message" http://localhost:3000/msg **/
-    app.post('/msg', function (req, res) {
-        // TRANSFORM THIS DATA.
-        var busstop = req.body.Body;
+    /**
+     * Computation, logs request values to the console.
+     *
+     * @param {Object} req - The request value.
+     **/
+    var logRequestDebuggingValues = function (req) {
         console.log("****DEBUGGING***");
         console.log(req.headers);
         console.log(req.query);
         console.log(req.body);
-        console.log("form",req.form);
+        console.log("form", req.form);
         console.log(req.params);
-        (isNaN(parseInt(req.body.Body)) ? 
-            getResponse('Hi, thanks for texting your HRT bus! Please text a ' 
-                 + 'stop number to find the next time your bus will come your '
-                 + 'way.') : 
-             // http://hrtb.us/#stops/0263
-            getStops(req.body.Body).then(getResponse)).then(res.send.bind(res));
-    });
+    };
+
+    /** 
+     * Makes API request to the HRTB.US API and transform the json results.
+     *
+     * @param {String} param - The parameter to get stops for.
+     *
+     * @return {Promise} A promise returning the stringified hrtb.us results,
+     *      passing them into the continuation.
+     **/
+    var getStops = function (param) {
+        return new bPromise(function (resolve, reject) {
+            r.get("http://api.hrtb.us/api/stop_times/" + param,
+                function (err, response, body) {
+                    var stops = body === '[]' || JSON.parse(body) === [] ?
+                        'Hmm, I cannot tell if that stop does not exist or if '
+                            + 'no buses will come to that stop in the near '
+                            + 'future.\nEither way, no buses will come to the '
+                            + 'stop you mentioned any time soon.' :
+                        transformSteps(body);
+                    console.log(stops);
+                    return resolve(stops);
+                });
+        }); 
+    };
+
+    /**
+     * Parse out the stops and return them in a human-readable format.
+     *
+     * @param {String} body - The body of the response.
+     *
+     * @return {String} The stops in a string format.
+     **/
+    var transformSteps = function (body) {
+        // Sort by routes and destinations.
+        return _.map(_.groupBy(JSON.parse(body), 'destination'),
+            function (stops) {
+                // EVMS/NORFOLK will be here in about 10, the next one in 15.
+                // NEWTOWN ROAD will be here in about 5 minutes and the next.
+                return _.reduce(stops, function (response, stop) {
+                    return { 
+                              '0': 'Light rail'
+                            , '3': 'Bus'
+                            , '4': 'Ferry' 
+                        }[stop.drop_off_type] + ' '  
+                        + stop.routeShortName + ' to ' + stop.destination 
+                        + ' will arrive in ' 
+                        + moment.utc(stop.arrival_time).diff(moment.utc()
+                            , 'minutes') 
+                        + ' minutes.';
+                }, '');
+            }).join('\n');
+    };
 
     /**
      * Returns a promise which returns an XML response of the given message.
@@ -71,65 +129,34 @@
         });
     };
 
-    /** 
-     * Makes API request to the HRTB.US API and transform the json results.
+    /**
+     * Predicate, holds when the body has an address.
      *
-     * @param {String} param - The parameter to get stops for.
+     * @param {String} body - The request body to recognise as an address.
      *
-     * @return {Promise} A promise returning the stringified hrtb.us results,
-     *      passing them into the continuation.
+     * @return {Boolean} Does the body have an address?
      **/
-    var getStops = function (param) {
-        return new bPromise(function (resolve, reject) {
-            r.get("http://api.hrtb.us/api/stop_times/" 
-                + (_.isUndefined(param) ? 
-                    '8004' :
-                    param),
-                function (err, response, body) {
-                    var stops = body === '[]' || JSON.parse(body) === [] ?
-                        'Hmm, I cannot tell if that stop does not exist or if no '
-                            + 'buses will come to that stop in the near future.\n'
-                            + 'Either way, no buses will come to the stop you '
-                            + 'mentioned any time soon.' :
-                        parseStops(body, resolve);
-                    console.log(stops);
-                    return resolve(stops);
-                });
-        }); 
+    var hasAddress = function (body) {
+        // TODO - Write address recogniser.
+        return false;
     };
 
     /**
-     * Parse out the steps, output them and run a continuation on them.
+     * Returns a message from the given address.
      *
-     * @param {String} body - The body of the response.
+     * @param {String} address - The address to return a Twilio message from.
      *
-     * @return {String} The stops in a string format.
+     * @return {Promise} The stop promise.
      **/
-    var parseStops = function (body) {
-        // Sort by routes and destinations.
-        return _.map(_.groupBy(JSON.parse(body), 'destination'),
-            function (stops) {
-                // EVMS/NORFOLK will be here in about 10, the next one in 15.
-                // NEWTOWN ROAD will be here in about 5 minutes and the next.
-                return _.reduce(stops, function (response, stop) {
-                    return { 
-                              '0': 'Light rail'
-                            , '3': 'Bus'
-                            , '4': 'Ferry' 
-                        }[stop.drop_off_type] + ' '  
-                        + stop.routeShortName + ' to ' + stop.destination + ' in ' 
-                        + moment.utc(stop.arrival_time).diff(moment.utc()
-                            , 'minutes') 
-                        + ' minutes.';
-                }, '');
-            }).join('\n');
+    var getAddress = function (address) {
+        return new bPromise(function (resolve, reject) {
+            // TODO - Write the code to return a stop from the given address.
+            return resolve(address);
+        });
     };
 
     app.set('port', process.env.PORT || 3000);
-
     app.listen(app.get('port'), function() {
         console.log('Express server listening on port ' + app.get('port'));
     });
-
-    getStops();
 }());
