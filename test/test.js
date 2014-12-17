6e2bd5750204bee4feecb6f32e45cfb5f8ed924b
stanley zheng <dev.stanzheng@gmail.com>
2014-12-17 09:07:35 -0500

    adding additional tests to be written

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require("assert");
+var request = require("request");
+
+/**
+* Test
+*
+* @param {Object} req - The request value.
+**/
+
+// #TODO Tests for
+// Bounding Boxes
+// Asking for help
+// Checking for input a known address
+// Checking for input a unknown/invalid address
+// Check for known stop number
+// Check for unknown/invalid stop number
