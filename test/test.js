213cdb6f99b7c8fc793270451846030a8c7e121c
stanley zheng <dev.stanzheng@gmail.com>
2014-12-17 16:39:37 -0500

    adding tests via supertest pattern, can make some general assertions on what to expect

--- test/test.js
+++ test/test.js
@@ -1,11 +1,31 @@
 var assert = require("assert");
-var request = require("request");
+var express = require("express");
+var r = require("supertest");
 
 /**
 * Test
 *
 * @param {Object} req - The request value.
 **/
+process.env.PORT = 4000;
+var app = require("../app.js");
+
+
+describe('General incoming request', function(){
+  it('respond with xml', function(done){
+    r(app)
+    .post('/msg')
+    .send({Body:"8004"})
+    .set('Accept', 'application/xml')
+    .expect(200, done);
+  });
+});
+
+function clientError(e) {
+  return e.code >= 400 && e.code < 500;
+}
+
+
 
 // #TODO Tests for
 // Bounding Boxes
