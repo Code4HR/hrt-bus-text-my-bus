bf75dfe0864f872da53912ee624a712b7c58c20b
stanley zheng <dev.stanzheng@gmail.com>
2014-12-18 15:05:33 -0500

    adding some tests for address compatibility

--- test/test.js
+++ test/test.js
@@ -14,7 +14,7 @@ var app = require("../app.js");
 
 describe('General incoming request', function(){
 
-  it('responds with xml', function(done){
+  it('should respond with xml', function(done){
     r(app)
     .post('/msg')
     .send({Body:"8004"})
@@ -26,7 +26,7 @@ describe('General incoming request', function(){
     });
   });
 
-  it('light rail stops are valid, testing 8004', function(done){
+  it('for a light rail stop named 8004', function(done){
     r(app)
     .post('/msg')
     .send({Body:"8004"})
@@ -42,11 +42,51 @@ describe('General incoming request', function(){
       parseString(xml, function (err, ouput) {
          var parse = ouput.Response.Message[0];
          parse.should.startWith('Light rail ');
-         //parse.should.have("Light rail");
       });
       done();
     });
   });
+  it('should decode a bus stop for an address in norfolk ', function(done){
+    r(app)
+    .post('/msg')
+    .send({Body:"111 Granby street "})
+    .set('Accept', 'application/xml')
+    .expect(200)
+    .end(function(err, res){
+      if (err) throw err;
+      should.not.exist(err);
+
+      // cleans utf8 parsing for xml in form
+      // see http://www.multiasking.com/blog/xml2js-sax-js-non-whitespace-before-first-tag/
+      var xml = res.text.replace("\ufeff", "");
+      parseString(xml, function (err, ouput) {
+        var parse = ouput.Response.Message[0];
+        parse.should.startWith('Light rail ');
+      });
+      done();
+    });
+  });
+  it('should decode a full address', function(done){
+    r(app)
+    .post('/msg')
+    .send({Body:"111 Granby street Norfolk VA 23508"})
+    .set('Accept', 'application/xml')
+    .expect(200)
+    .end(function(err, res){
+      if (err) throw err;
+      should.not.exist(err);
+
+      // cleans utf8 parsing for xml in form
+      // see http://www.multiasking.com/blog/xml2js-sax-js-non-whitespace-before-first-tag/
+      var xml = res.text.replace("\ufeff", "");
+      parseString(xml, function (err, ouput) {
+        var parse = ouput.Response.Message[0];
+        parse.should.startWith('Light rail ');
+      });
+      done();
+    });
+  });
+
 });
 
 
@@ -57,3 +97,4 @@ describe('General incoming request', function(){
 // Checking for input a unknown/invalid address
 // Check for known stop number
 // Check for unknown/invalid stop number
+// Check for addresses in 7 cities with generic typical names
